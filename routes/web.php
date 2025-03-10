552681c4904b47027f148062376a91580977626f
AzizbekZiyodullayev2024 <ziyodulayeva9@gmail.com>
2025-03-10 12:01:25 +0500

    Added moonshine

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/login', function () {
+    return view('Auth.login');
+});
+
+Route::get('/register', function () {
+    return view('Auth.register');
+});
+
+Route::get('/contact_us', function () {
+    return view('contact_us');
 });
